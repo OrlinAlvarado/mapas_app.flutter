9ad4d5a62a2fdfccefc7ed45e8d419e29987340a
Orlin Alvarado <oalvaradomorales@gmail.com>
2020-11-01 21:32:54 -0600

    Publicacion MAPA funcional

---
+++ lib/bloc/mi_ubicacion/mi_ubicacion_bloc.dart
@@ -0,0 +1,38 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart' show LatLng;
+import 'package:meta/meta.dart';
+
+part 'mi_ubicacion_event.dart';
+part 'mi_ubicacion_state.dart';
+
+class MiUbicacionBloc extends Bloc<MiUbicacionEvent, MiUbicacionState> {
+  MiUbicacionBloc() : super(MiUbicacionState());
+  
+  StreamSubscription<Position> _streamSubscription;
+  
+  void iniciarSeguimiento(){
+    this._streamSubscription = Geolocator.getPositionStream(
+      desiredAccuracy: LocationAccuracy.high,
+      distanceFilter: 10
+    ).listen(( Position position ){
+      final nuevaUbicacion = LatLng(position.latitude, position.longitude);
+      add( OnUbicacionCambio( nuevaUbicacion ));
+    });
+  }
+  
+  void cancelarSeguimiento() {
+    _streamSubscription?.cancel();
+  }
+  @override
+  Stream<MiUbicacionState> mapEventToState(MiUbicacionEvent event) async* {
+    if( event is OnUbicacionCambio ){
+      yield state.copyWith(
+        existeUbicacion: true,
+        ubicacion: event.ubicacion
+      );
+    }
+  }
+}
---
+++ lib/pages/mapa_page.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:mapas_app/bloc/mapa/mapa_bloc.dart';
+import 'package:mapas_app/bloc/mi_ubicacion/mi_ubicacion_bloc.dart';
+import 'package:mapas_app/widgets/widgets.dart';
+
+class MapaPage extends StatefulWidget {
+
+  @override
+  _MapaPageState createState() => _MapaPageState();
+}
+
+class _MapaPageState extends State<MapaPage> {
+  
+  @override
+  void initState() {
+    context.bloc<MiUbicacionBloc>().iniciarSeguimiento();
+    super.initState();
+  }
+  
+  @override
+  void dispose() {
+    context.bloc<MiUbicacionBloc>().cancelarSeguimiento();
+    super.dispose();
+  }
+@override
+  Widget build(BuildContext context) {
+   return Scaffold(
+      appBar: AppBar(
+           title: Text('Mapa', textAlign: TextAlign.center,)
+      ),
+      body: BlocBuilder<MiUbicacionBloc, MiUbicacionState>(
+        builder: (_, state) => crearMapa(state)
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          BtnUbicacion(),
+          BtnSeguirUbicacion(),
+          BtnMiRuta(),
+        ],
+      ),
+    );
+  }
+  
+  Widget crearMapa( MiUbicacionState state ){
+    if( !state.existeUbicacion) return Center(child: Text('Ubicando...'));
+    
+    final mapaBloc = BlocProvider.of<MapaBloc>(context);
+    
+    mapaBloc.add(OnNuevaUbicacion( state.ubicacion));
+    //return Text('${ state.ubicacion.latitude }, ${ state.ubicacion.longitude }');
+    final cameraPosition = new CameraPosition(
+      target: state.ubicacion,
+      zoom: 15
+    );
+    return GoogleMap(
+      initialCameraPosition: cameraPosition,
+      myLocationEnabled: true,
+      myLocationButtonEnabled: false,
+      zoomControlsEnabled: false,
+      onMapCreated: mapaBloc.initMapa,
+      polylines: mapaBloc.state.polylines.values.toSet(),
+      onCameraMove: ( cameraPosition ) {
+        mapaBloc.add( OnMovioMapa( cameraPosition.target ));
+      }
+    );
+  }
+}
